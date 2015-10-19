be9abd0a86072dc8a8131e224768ce85793a500d
jletria <jmletria@gmail.com>
2015-10-20 00:31:51 +0100

    Implemented vertex array based grid prototype

--- SFMLSandbox/SFMLGui/Form1.Designer.cs
+++ SFMLSandbox/SFMLGui/Form1.Designer.cs
@@ -25,6 +25,7 @@
         private void InitializeComponent() {
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // panel1
@@ -36,24 +37,34 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(436, 538);
+            this.button1.Location = new System.Drawing.Point(437, 518);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 518);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(530, 573);
+            this.ClientSize = new System.Drawing.Size(526, 550);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -61,6 +72,7 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- SFMLSandbox/SFMLGui/Form1.cs
+++ SFMLSandbox/SFMLGui/Form1.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -14,49 +13,114 @@ using System.Windows.Forms;
 namespace SFMLGui {
     public partial class Form1 : Form {
 
+        
+        RectangleShape shape;
+
+
         RenderWindow window;
-        CircleShape shape;
-        System.Timers.Timer timer;
+        Thread drawingThread;
+        VertexArray va;
 
         bool initialized = false;
 
+        Dictionary<string, Color> colors;
+
         public Form1() {
             InitializeComponent();
-            timer = new System.Timers.Timer(32);
-            timer.Elapsed += Timer_Elapsed;
+            colors = new Dictionary<string, Color>();
+            drawingThread = new Thread(new ThreadStart(DrawingThread));
+            PopulateColors();
+        }
 
-            Thread drawingThread = new Thread(new ThreadStart(DrawingThread));
-
+        private void PopulateColors() {
+            colors.Clear();
+            List<string> skipColors = new List<string>();
+            bool invalidColors = true;
+            System.Drawing.Color tmpColor;
+            foreach (System.Drawing.KnownColor c in Enum.GetValues(typeof(System.Drawing.KnownColor)).Cast<System.Drawing.KnownColor>()) {
+                if (c == System.Drawing.KnownColor.ButtonFace) break;
+                //if (c == System.Drawing.KnownColor.Black) continue;
+                if (!invalidColors && !skipColors.Contains(c.ToString())) {
+                    tmpColor = System.Drawing.Color.FromKnownColor(c);
+                    colors.Add(c.ToString(), new Color(tmpColor.R, tmpColor.G, tmpColor.B, tmpColor.A));
+                }
+                if (c == System.Drawing.KnownColor.Transparent) invalidColors = false;
+            }
         }
 
         private void button1_Click(object sender, EventArgs e) {
-            
-            timer.Start();
             //Draw();
+            if (!initialized) {
+                initialized = true;
+                //window = new RenderWindow(new VideoMode(500, 500), "");
+                window = new RenderWindow(panel1.Handle);
+                window.Clear();
+
+                uint GridWidth = 10;
+                uint GridHeight = 10;
+                uint SquareWidth = (500 / GridWidth);
+                uint SquareHeight = (500 / GridHeight);
+
+                va = new VertexArray(PrimitiveType.Quads, GridWidth * GridHeight * 4);
+                /*
+                for(int i = 0; i < va.VertexCount; i++) {
+                    va[i].
+                }
+                */
+                List<Color> colorList = colors.Values.ToList();
+                uint xcursor = 0, ycursor = 0;
+                for (int y = 0; y < GridHeight; y++) {
+                    for (int x = 0; x < GridWidth; x++) {
+                        va.Append(new Vertex(new SFML.System.Vector2f(xcursor, ycursor), colorList[x + y]));
+                        va.Append(new Vertex(new SFML.System.Vector2f(xcursor + SquareWidth, ycursor), colorList[x + y]));
+                        va.Append(new Vertex(new SFML.System.Vector2f(xcursor + SquareWidth, ycursor + SquareHeight), colorList[x + y]));
+                        va.Append(new Vertex(new SFML.System.Vector2f(xcursor, ycursor + SquareHeight), colorList[x + y]));
+                        xcursor += SquareWidth;
+                    }
+                    xcursor = 0;
+                    ycursor += SquareHeight;
+                }
+                shape = new RectangleShape(new SFML.System.Vector2f(10, 10));
+                shape.FillColor = SFML.Graphics.Color.Red;
+                shape.OutlineThickness = 1;
+                shape.OutlineColor = SFML.Graphics.Color.Blue;
+                shape.Position = new SFML.System.Vector2f(0,0);
+            }
+            drawingThread.Start();
         }
 
         private void Draw() {
-            this.window.Draw(shape);
+            this.window.Draw(va);
             this.window.Display();
         }
 
-        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e) {
-
-        }
 
         private void DrawingThread() {
-            if (!initialized) {
-                initialized = true;
-                window = new RenderWindow(panel1.Handle);
-                window.Clear();
-                shape = new CircleShape(50);
-                shape.FillColor = SFML.Graphics.Color.Red;
-                shape.OutlineThickness = 10;
-                shape.OutlineColor = SFML.Graphics.Color.Blue;
+            List<Color> colorList = colors.Values.ToList();
+            for (int i = 0; true; i++) {
+                try {
+                    /*if (i == colorList.Count) i = 0;
+                    shape.FillColor = colors["MidnightBlue"];
+                    shape.Position = new SFML.System.Vector2f(shape.Position.X + 5, shape.Position.Y + 5);*/
+                    Invoke(new Action(() => { Draw(); }));
+                    System.Threading.Thread.Sleep(13);
+                }
+                catch (Exception e) { break; }
             }
-            while (true) {
+        }
 
-                Draw();
+
+        delegate void SetTextCallback(string text);
+        private void SetText(string text) {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.textBox1.InvokeRequired) {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else {
+                this.textBox1.Text = text;
             }
         }
     }
